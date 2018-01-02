9c91a76c1b5c9fc7e16966073ee0d4da6e9abd68
SamMorganJiang <Sam.Morgan.Jiang@outlook.com>
2018-01-03 01:01:40 +0800

    Shell

---
+++ src/cn/sort/Shell.java
@@ -0,0 +1,50 @@
+package cn.sort;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Shell {
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        int h = 1;
+        while (h < N / 3) h = h * 3 + 1;
+        while (h >= 1) {
+            for (int i = h; i < N; i++) {
+                for (int j = i; j >= h && less(a[j], a[j - h]); j -= h) {
+                    swap(a, j, j - h);
+                }
+            }
+            h = h / 3;
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void swap(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show(Comparable[] a) {
+        for (Comparable A : a) StdOut.print(A + " ");
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] s = new String[]{
+                "S", "H", "E", "L", "L", "S", "O", "R",
+                "T", "E", "X", "A", "M", "P", "L", "E"
+        };
+        sort(s);
+        assert isSorted(s);
+        show(s);
+    }
+}
--- src/cn/sort/Sort.java
+++ src/cn/sort/Sort.java
@@ -9,6 +9,7 @@ class SortCompare {
         Stopwatch timer = new Stopwatch();
         if (alg.equals("Selection")) Selection.sort(a);
         if (alg.equals("Insertion")) Insertion.sort(a);
+        if (alg.equals("Shell")) Shell.sort(a);
         return timer.elapsedTime();
     }
 
@@ -32,14 +33,17 @@ public class Sort {
 
         String alg1 = "Insertion";
         String alg2 = "Selection";
+        String alg3 = "Shell";
 
-        int N = 1000;
+        int N = 10000;
         int T = 100;
 
         double t1 = SortCompare.timeRandomInput(alg1, N, T);
         double t2 = SortCompare.timeRandomInput(alg2, N, T);
+        double t3 = SortCompare.timeRandomInput(alg3, N, T);
 
-        StdOut.printf("For %d random Doubles\n  %s is", N, alg1);
-        StdOut.printf(" %.1f times faster than %s\n", t2 / t1, alg2);
+        StdOut.printf("For %d random Doubles\n", N);
+        StdOut.printf("  %s is %.1f times faster than %s\n", alg1, t2 / t1, alg2);
+        StdOut.printf("  %s is %.1f times faster than %s\n", alg3, t1 / t3, alg1);
     }
 }
