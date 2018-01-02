a205553d207062fccb3d6d53668c86f9b3c6a926
SamMorganJiang <Sam.Morgan.Jiang@outlook.com>
2018-01-03 00:15:20 +0800

    Insertion and SortCompare

---
+++ src/cn/sort/Insertion.java
@@ -0,0 +1,42 @@
+package cn.sort;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Insertion {
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        for (int i = 1; i < N; i++) {
+            for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {
+                swap(a, j, j - 1);
+            }
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void swap(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show(Comparable[] a) {
+        for (Comparable A : a) StdOut.print(A + " ");
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] s = new String[]{"t", "s", "h", "b", "o"};
+        sort(s);
+        assert isSorted(s);
+        show(s);
+    }
+}
--- src/cn/sort/Sort.java
+++ src/cn/sort/Sort.java
@@ -1,7 +1,45 @@
 package cn.sort;
 
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.Stopwatch;
+
+class SortCompare {
+    public static double time(String alg, Comparable[] a) {
+        Stopwatch timer = new Stopwatch();
+        if (alg.equals("Selection")) Selection.sort(a);
+        if (alg.equals("Insertion")) Insertion.sort(a);
+        return timer.elapsedTime();
+    }
+
+    public static double timeRandomInput(String alg, int N, int T) {
+        double total = 0.0;
+        Double[] a = new Double[N];
+
+        for (int i = 0; i < T; i++) {
+            for (int j = 0; j < N; j++) {
+                a[j] = StdRandom.uniform();
+            }
+            total += time(alg, a);
+        }
+        return total;
+    }
+}
+
 public class Sort {
     public static void main(String[] args) {
-        System.out.println("Sorting algorithm test");
+        System.out.println("Sorting algorithm test:");
+
+        String alg1 = "Insertion";
+        String alg2 = "Selection";
+
+        int N = 1000;
+        int T = 100;
+
+        double t1 = SortCompare.timeRandomInput(alg1, N, T);
+        double t2 = SortCompare.timeRandomInput(alg2, N, T);
+
+        StdOut.printf("For %d random Doubles\n  %s is", N, alg1);
+        StdOut.printf(" %.1f times faster than %s\n", t2 / t1, alg2);
     }
 }
